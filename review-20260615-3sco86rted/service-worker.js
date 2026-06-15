e3cb246df9846e53240ce0c504382029f799d115
mi-hiro <color.apple.color.apple@gmail.com>
2026-06-15 17:38:21 +0900

    Add temporary review viewer link

---
+++ review-20260615-3sco86rted/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "miyakonojo-land-viewer-v41";
+const SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./route-values.json",
+  "./fixed-asset-route-values.json",
+  "./hazard-zones.json",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") {
+    return;
+  }
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+        return response;
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
